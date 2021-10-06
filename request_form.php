36735a274f00bd39c68e81df5f69ff08aa1d9f85
Celestial <jbernadas@ssct.edu.ph>
2021-10-06 19:12:25 +0800

    add QR generator
    add phpqrcode lib for qr generation

--- request_form.php
+++ request_form.php
@@ -1,67 +1,65 @@
-<?php include 'index.php'; ?>
+<?php include 'index.php';
+include './phpqrcode/qrlib.php';
+
+function getName($name)
+{
+    $find = '@';
+    $pos = strpos($name, $find);
+    $username = substr($name, 0, $pos);
+    return $username;
+}
+
+if (isset($_POST['submit'])) {
+    $tempDir = 'temp/';
+    $docsType = $_POST['docs-type'];
+    $name = $_POST['name'];
+    $description = $_POST['description'];
+    $filename = getName($name);
+    $codeContents = 'documentType:' . $docsType . '?name=' . urlencode($name) . '&body=' . urlencode($description);
+    QRcode::png($codeContents, $tempDir . '' . $filename . '.png', QR_ECLEVEL_L, 5);
+}
+
+?>
+
 <div class="main-panel">
     <div class="content-wrapper">
         <div class="row">
             <div class="col-md-12 grid-margin stretch-card">
                 <div class="card">
-                    <div class="card-body">
-                        <form class="forms-sample">
+                    <div class="card-body" style="display: flex; flex-wrap: wrap; justify-content: space-between;">
+                        <form class="forms-sample" style="width: 70%; margin: 0 auto" method="post" action="">
                             <div class="form-group">
                                 <label for="exampleInputUsername1">Document Type</label>
-                                <select class="form-control" name="" id="">
-                                    <option value="volvo" selected disabled>-- Select --</option>
-                                    <option value="">Clearance</option>
-                                    <option value="">Resign</option>
+                                <select class="form-control" name="docs-type" id="">
+                                    <option selected disabled>-- Select --</option>
+                                    <option value="Clearance">Clearance</option>
+                                    <option value="Resign">Resign</option>
                                 </select>
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Complete Name</label>
-                                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Complete Name" readonly>
+                                <input value="<?php echo @$name; ?>" name="name" type="text" class="form-control" id="exampleInputEmail1" placeholder="Complete Name">
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Description</label>
-                                <textarea name="" id="" cols="30" rows="10" class="form-control" placeholder="Enter description"></textarea>
+                                <textarea value="<?php echo @$description; ?>" name="description" id="" cols="30" rows="10" class="form-control" placeholder="Enter description"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" name="submit" class="btn btn-primary btn-info mr-2" style="width: 100%" />
                             </div>
                         </form>
-                        <button type="submit" class="btn btn-info mr-2" data-toggle="modal" data-target="#myModal1">Submit</button>
+                        <div class="qr-field" style="margin: 15px auto">
+                            <h2 style="text-align:center">Your QR Code</h2>
+                            <div>
+                                <div style="border:2px solid black; width:210px; height:210px;">
+                                    <?php echo '<img src="temp/' . @$filename . '.png" style="width:200px; height:200px;"><br>'; ?>
+                                </div>
+                                <a class="btn btn-primary btn-info mr-2" style="width:210px; margin:5px 0;" href="download.php?file=<?php echo $filename; ?>.png ">Download QR Code</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<!--Start of Modal-->
-<div class="modal fade modal-mini modal-primary" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header justify-content-center">
-                <div class="modal-profile">
-                    <p>Search result for</p>
-                    <img src="./images/others/no-image-available.png" id="modal-qr-img" width="250px" height="250px" style="margin: 25px 25px; border:1px white grey" alt="qr-code-image" />
-                    <p>Type: <span style="font-weight: bold;">Clearance</span></p>
-                    <p>Name: <span style="font-weight: bold;">Dandy Makabudbud</span></p>
-                    <p>Description: <span style="font-weight: bold;">Please notice me</span></p>
-                    <hr>
-                    <ul>
-                        <li>
-                            <div>
-                                3rd May 2020<br> 7:00 PM
-                            </div>
-                            <div class="item-title" style="color: green;">QR Code was generated</div>
-                        </li>
-                        <li>
-                            <div>
-                                19th May 2020<br> 3:00 PM
-                            </div>
-                            <div class="item-title" style="color: green;">Received and Approved</div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-link btn-simple" data-dismiss="modal">Close</button>
-            </div>
-        </div>
-    </div>
-</div>
-<!--End of Modal-->
